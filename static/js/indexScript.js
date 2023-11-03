350e322b6409f52fdd37dfe052d4e731f2f6c250
highxshell <121538758+highxshell@users.noreply.github.com>
2023-11-03 15:40:13 +0600

    adding event listeners

--- static/js/indexScript.js
+++ static/js/indexScript.js
@@ -6,3 +6,19 @@ document.getElementById("Register").addEventListener("click", function () {
     alert("You are too young to use our store.");
   }
 });
+
+document.getElementById("Register").onmouseover = function () {
+  document.getElementById("Register").style.fontSize = "200%";
+};
+
+document.getElementById("Register").onmouseout = function () {
+  document.getElementById("Register").style.fontSize = "125%";
+};
+
+document.getElementById("Login").onmouseover = function () {
+  document.getElementById("Login").style.fontSize = "200%";
+};
+
+document.getElementById("Login").onmouseout = function () {
+  document.getElementById("Login").style.fontSize = "125%";
+};
