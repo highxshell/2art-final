b70a04abdba40d1f2617064c77224b1361eca66d
highxshell <121538758+highxshell@users.noreply.github.com>
2023-11-04 13:12:57 +0600

    Add files via upload

---
+++ admin/database/dbconfig.php
@@ -0,0 +1,50 @@
+<?php
+
+$server_name = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "adminpanel";
+
+$connection = mysqli_connect($server_name,$db_username,$db_password,$db_name);
+
+if(!$connection)
+{
+    die("Connection failed: " . mysqli_connect_error());
+    echo '
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mr-auto ml-auto text-center py-5 mt-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1 class="card-title bg-danger text-white"> Database Connection Failed </h1>
+                            <h2 class="card-title"> Database Failure</h2>
+                            <p class="card-text"> Please Check Your Database Connection.</p>
+                            <a href="#" class="btn btn-primary">:( </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ';
+}
+if(isset($_POST['delete_btn']))
+{
+    $id = $_POST['delete_id'];
+
+    $query = "DELETE FROM register WHERE id='$id' ";
+    $query_run = mysqli_query($connection, $query);
+
+    if($query_run)
+    {
+        $_SESSION['status'] = "Your Data is Deleted";
+        $_SESSION['status_code'] = "success";
+        header('Location: register.php'); 
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Data is NOT DELETED";       
+        $_SESSION['status_code'] = "error";
+        header('Location: register.php'); 
+    }    
+}
+?>
